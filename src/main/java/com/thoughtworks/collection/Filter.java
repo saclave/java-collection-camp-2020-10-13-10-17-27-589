597448ccf4c612360ebc1ff24499a5a2a913a6d7
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-14 11:54:30 +0800

    [Java Collection][Vea] feature - Java Collection Activity

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import org.apache.commons.lang3.NotImplementedException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -12,14 +13,20 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream()
+                .filter(array -> array % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream()
+                .filter(array -> array % 3 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return firstList.stream()
+                .filter(secondList::contains)
+                .collect(Collectors.toList());
     }
 }
